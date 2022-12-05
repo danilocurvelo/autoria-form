0bb7ad42463825325b36cedc1c36c2479596b63f
Danilo Curvelo <danilocurvelo@gmail.com>
2022-12-05 13:46:00 +0000

    test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const app = require('express')();
 const { v4 } = require('uuid');
+const postService = require('../services/post-service');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+//   const path = `/api/item/${v4()}`;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    const response = JSON.stringify(postService.all())
+    res.send(response)
 });
 
 app.get('/api/item/:slug', (req, res) => {
