6b4ecf451af98ff050ab87be9d784ebdaa6a4ea6
Danilo Curvelo <danilocurvelo@gmail.com>
2022-12-05 10:25:06 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+const express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const port = process.env.PORT || 3000
+var indexRouter = require('../routes/index');
+var postsRouter = require('../routes/posts')
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/post', postsRouter);
+app.use('/blog-post', postsRouter);
+app.use('/blog', postsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app;
